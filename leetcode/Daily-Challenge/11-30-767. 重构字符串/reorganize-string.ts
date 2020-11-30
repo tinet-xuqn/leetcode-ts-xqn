c9c4cd01f4f62e9dea8d0e0e86a1e2f58a8aa57c
xuqineng <379664090@qq.com>
2020-11-30 17:59:54 +0800

    11-30

---
+++ leetcode/Daily-Challenge/11-30-767. 重构字符串/reorganize-string.ts
@@ -0,0 +1,27 @@
+function reorganizeString(S: string): string {
+  const l = S.length;
+  const obj = {};
+  let max = 0;
+  for (let i = 0; i < S.length; i++) {
+    if (obj[S[i]]) {
+      obj[S[i]]++
+    } else {
+      obj[S[i]] = 1
+    }
+    max = max > obj[S[i]] ? max : obj[S[i]]
+  }
+  const allowMax = (l + 1) / 2;
+  if (max > Math.floor(allowMax)) {
+    return ''
+  }
+  const arr = []
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      arr.push({
+        label: key,
+        value: obj[key]
+      });
+    }
+  }
+  
+};
