bc7c4ddf0bc28a515654f029b0fc336b89bda401
xuqineng <379664090@qq.com>
2021-02-22 10:17:32 +0800

    2-21/滑动窗口

--- leetcode/Daily-Challenge/2021/2-21-1438. 绝对差不超过限制的最长连续子数组/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.ts
+++ leetcode/Daily-Challenge/2021/2-21-1438. 绝对差不超过限制的最长连续子数组/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.ts
@@ -1,25 +1,66 @@
 function longestSubarray(nums: number[], limit: number): number {
-  let len = 0, minIndex = 0, maxIndex = 0, left = 0, right = 0, curD = [];
+  let len = -1,
+      minQueue: number[] = [],
+      maxQueue: number[] = [],
+      left = 0,
+      right = 0;
   while (right < nums.length) {
-    if (nums[right] > nums[maxIndex]) {
-      maxIndex = right;
-      while (nums[maxIndex] - nums[minIndex] > limit) {
-        curD = nums.slice(minIndex + 1, right + 1);
-        let min = Math.min(...curD);
-        minIndex = nums.indexOf(min, minIndex + 1);
-        left = minIndex;
+    while (minQueue.length && minQueue[minQueue.length - 1] > nums[right]) {
+      minQueue.pop();
+    }
+    minQueue.push(nums[right]);
+
+    while (maxQueue.length && maxQueue[maxQueue.length - 1] < nums[right]) {
+      maxQueue.pop();
+    }
+    maxQueue.push(nums[right]);
+
+    while (maxQueue[0] - minQueue[0] > limit) {
+      if (maxQueue[0] === nums[left]) {
+        maxQueue.shift();
       }
-    } else if (nums[right] < nums[minIndex]) {
-      minIndex = right;
-      while (nums[maxIndex] - nums[minIndex] > limit) {
-        curD = nums.slice(maxIndex + 1, right + 1);
-        let max = Math.max(...curD);
-        maxIndex = nums.indexOf(max, maxIndex + 1);
-        left = maxIndex;
+      if (minQueue[0] === nums[left]) {
+        minQueue.shift();
       }
+      left++;
     }
     right++;
     len = Math.max(len, right - left);
   }
   return len;
 };
+
+function longestSubarray1(nums: number[], limit: number): number {
+  let len = -1,
+      minQueue: number[] = [],
+      maxQueue: number[] = [],
+      left = 0,
+      right = 0;
+  while (right < nums.length) {
+    while (minQueue.length && minQueue[minQueue.length - 1] > nums[right]) {
+      minQueue.pop();
+    }
+    minQueue.push(nums[right]);
+
+    while (maxQueue.length && maxQueue[maxQueue.length - 1] < nums[right]) {
+      maxQueue.pop();
+    }
+    maxQueue.push(nums[right]);
+
+    // 保持窗口大小不变
+    if (maxQueue[0] - minQueue[0] > limit) {
+      if (maxQueue[0] === nums[left]) {
+        maxQueue.shift();
+      }
+      if (minQueue[0] === nums[left]) {
+        minQueue.shift();
+      }
+      left++;
+    } else {
+      len = right - left + 1;
+    }
+    right++;
+  }
+  return len;
+};
+
