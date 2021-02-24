a4ebdda1077d94d7de0050c5820554bc0f34337d
xuqineng <379664090@qq.com>
2021-02-24 10:27:47 +0800

    2-23/滑动窗口

--- leetcode/Daily-Challenge/2021/2-23-1052. 爱生气的书店老板/grumpy-bookstore-owner.ts
+++ leetcode/Daily-Challenge/2021/2-23-1052. 爱生气的书店老板/grumpy-bookstore-owner.ts
@@ -1,5 +1,22 @@
 function maxSatisfied(customers: number[], grumpy: number[], X: number): number {
-  let res: number = 0;
-  
+  let res: number = 0, extraRes: number = 0, curExtra: number = 0;
+  for (let i = 0; i < customers.length; i++) {
+    res += customers[i] * grumpy[i];
+  }
+  for (let i = 0; i < X; i++) {
+    if (grumpy[i] === 0) {
+      extraRes += customers[i];
+    }
+  }
+  curExtra = extraRes;
+  for (let i = X; i < customers.length; i++) {
+    if (grumpy[i - X] === 0) {
+      curExtra = curExtra - customers[i];
+    }
+    if (grumpy[X] === 0) {
+      curExtra = curExtra + customers[i];
+    }
+    extraRes = Math.max(extraRes, curExtra);
+  }
   return res;
 };
