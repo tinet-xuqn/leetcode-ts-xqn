20ae82a84f62affc62889a56afc10710916facfe
xuqineng <379664090@qq.com>
2021-03-18 15:07:37 +0800

    3/18/反转链表

---
+++ leetcode/Daily-Challenge/2021/3-18-92. 反转链表 II/reverse-linked-list-ii.ts
@@ -0,0 +1,62 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
+  if (!head) {
+    return null;
+  }
+  if (left === right) {
+    return head;
+  }
+  let curNode = head;
+  let queueL: ListNode[] = [];
+  let queueReverse: ListNode[] = [];
+  let queueR: ListNode[] = [];
+  let cur = 1;
+  while (curNode) {
+    if (cur < left) {
+      queueL.push(curNode);
+    } else if (cur > right) {
+      queueR.push(curNode);
+    } else {
+      queueReverse.push(curNode);
+    }
+    curNode = curNode.next;
+    cur++;
+  }
+  const res: ListNode | null = new ListNode();
+  let lastNode = res;
+
+  console.log(queueL, queueReverse, queueR);
+  while (queueL.length) {
+    lastNode.next = queueL.shift();
+    lastNode = lastNode.next;
+  }
+  while (queueReverse.length) {
+    lastNode.next = queueReverse.pop();
+    lastNode = lastNode.next;
+  }
+  while (queueR.length) {
+    lastNode.next = queueR.shift();
+    lastNode = lastNode.next;
+  }
+  lastNode.next = null;
+  return res.next;
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let pre: ListNode | null = null;
+  let cur = head;
+  while (cur) {
+    const next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
+  return pre;
+};
