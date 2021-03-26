a8c8b7c79272a9575a001f2d446df6efd78bdee6
xuqineng <379664090@qq.com>
2021-03-26 16:32:38 +0800

    3/26/链表去重

---
+++ leetcode/Daily-Challenge/2021/3-26-83. 删除排序链表中的重复元素/remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,22 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return head;
+  }
+  let cur = head;
+  while (cur.next) {
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+  return head;
+};
