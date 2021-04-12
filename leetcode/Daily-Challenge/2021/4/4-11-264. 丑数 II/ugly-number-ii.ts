361bde107045fc001995137a4ef67b19ec2d291c
xuqineng <379664090@qq.com>
2021-04-12 17:32:20 +0800

    4/12/丑数，三指针

--- leetcode/Daily-Challenge/2021/4/4-11-264. 丑数 II/ugly-number-ii.ts
+++ leetcode/Daily-Challenge/2021/4/4-11-264. 丑数 II/ugly-number-ii.ts
@@ -1,3 +1,21 @@
 function nthUglyNumber(n: number): number {
-  
+  const uArr: number[] = new Array(n + 1).fill(0);
+  uArr[1] = 1;
+  let p2 = 1, p3 = 1, p5 = 1;
+  for (let i = 2; i <= n; i++) {
+    const num2 = uArr[p2] * 2;
+    const num3 = uArr[p3] * 3;
+    const num5 = uArr[p5] * 5;
+    uArr[i] = Math.min(num2, num3, num5);
+    if (uArr[i] === num2) {
+      p2++;
+    }
+    if (uArr[i] === num3) {
+      p3++;
+    }
+    if (uArr[i] === num5) {
+      p5++;
+    }
+  }
+  return uArr[n];
 };
--- leetcode/Daily-Challenge/2021/4/4-12-179. 最大数/largest-number.ts
+++ leetcode/Daily-Challenge/2021/4/4-12-179. 最大数/largest-number.ts
@@ -1,3 +1,9 @@
 function largestNumber(nums: number[]): string {
-  
+  const res = nums.sort((a, b) => {
+    if (a + '' + b > b + '' + a) {
+      return -1;
+    }
+    return 1;
+  }).join('');
+  return res.startsWith('0') ? '0' : res;
 };
