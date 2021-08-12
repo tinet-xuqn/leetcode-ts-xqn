ef3a4e24d160c3ce8d5659d9c35d21de0a176242
xuqineng <379664090@qq.com>
2021-08-12 15:27:32 +0800

    8/12/待看

---
+++ leetcode/Daily-Challenge/2021/8/8-12-516. 最长回文子序列/longest-palindromic-subsequence.ts
@@ -0,0 +1,29 @@
+const longestPalindromeSubseq = s => {
+  // 字符串长度
+  const len = s.length;
+  // 创建dp数组
+  const dp = new Array(len).fill(0).map(x => new Array(len).fill(0));
+  // dp数组初始化，矩阵主对角线都为1
+  for (let i = 0; i < len; i++) dp[i][i] = 1;
+
+  // 遍历顺序，从下往上，从左往右
+  for (let i = len - 1; i >= 0; i--) {
+      for (let j = i + 1; j < len; j++) {
+          // 根据s[i]和s[j]的关系更新dp[i][j]
+          if (s[i] === s[j]) {
+              // 头尾相等，则dp[i][j]等于减去头尾的数量+2
+              dp[i][j] = dp[i + 1][j - 1] + 2;
+          } else {
+              // 头尾不相等，则dp[i][j]等于减去头或减去尾的最大值
+              dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+          }
+      }
+  }
+  // 返回字符串的头到尾的数量
+  return dp[0][len - 1];
+};
+
+// 作者：lzxjack
+// 链接：https://leetcode-cn.com/problems/longest-palindromic-subsequence/solution/dong-tai-gui-hua-javascriptban-by-lzxjac-qvb8/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
