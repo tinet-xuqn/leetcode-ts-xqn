6c1370f6a5f44aa3fc71c117cef08d0e0319b351
xuqineng <379664090@qq.com>
2022-06-21 15:25:21 +0800

    6-21遍历替换

---
+++ leetcode/Daily-Challenge/2022/6/6-21-1108. IP 地址无效化/defanging-an-ip-address.ts
@@ -0,0 +1,11 @@
+function defangIPaddr(address: string): string {
+  let res: string = ''
+  for (let i = 0; i < address.length; i++) {
+    if (address[i] === '.') {
+      res += '[.]'
+    } else {
+      res += address[i]
+    }
+  }
+  return res
+};
